6eedcfcd060bdaab6e264178e4edeb77cd97019c
Stephanie <esceeaitchyu@gmail.com>
2018-12-10 09:18:29 -0800

    Complete checkpoint 2 assignment

---
+++ library-data.sql
@@ -0,0 +1,12 @@
+CREATE TABLE books (
+    id integer,
+    title text,
+    author text 
+);
+
+INSERT INTO books (id, title, author) 
+    VALUES
+    (1259, 'Eloquent Ruby', 'Russell A. Olson'),
+    (1593, 'JavaScript: The Good Parts', 'Douglas Crockford'),
+    (8982, 'Designing Object-Oriented Software', 'Rebecca Wirfs-Brock'),
+    (7265, 'Practical Object-Oriented Design in Ruby', 'Sandi Metz');
